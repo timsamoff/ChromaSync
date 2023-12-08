531707b0b566687e84993316f8c55f6281778f7b
Tim Samoff <tim@samoff.com>
2023-12-08 11:40:17 -0800

    Version 2.1
    Made color changing real-time; added the keyword "stop."

--- ChromaSync/Assets/Scripts/IncrementValues.cs
+++
@@ -1,6 +0,0 @@
-public static class IncrementValues
-{
-    public static int RedIncrement { get; set; }
-    public static int GreenIncrement { get; set; }
-    public static int BlueIncrement { get; set; }
-}
